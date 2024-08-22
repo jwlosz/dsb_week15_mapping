6768b91c6b6d415075077211a4e4231b74a5cd67
jwlosz <jwloszek97@gmail.com>
2024-08-21 20:11:12 -0400

    resub

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -30,7 +30,7 @@ function chooseColor(depth) {
 
 
 
-function createMap(data, geodata) {
+function createMap(data) {
     //being with base layers
     let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
